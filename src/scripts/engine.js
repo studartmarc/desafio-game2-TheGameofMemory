a4309b6330b941843af3e265bbbddbd822aa63f8
Marcelo Studart <studartmarc@gmail.com>
2024-02-17 19:02:29 -0300

    implementação da condição de vitória. identificação de melhorias no jogo. totalmente funcional de acordo com instruções do curso

---
+++ src/scripts/engine.js
@@ -0,0 +1,57 @@
+const animalsEmojis = [
+    "🦎",
+    "🦎",
+    "🐍",
+    "🐍",
+    "🦥",
+    "🦥",
+    "🦔",
+    "🦔",
+    "🐿",
+    "🐿",
+    "🐆",
+    "🐆",
+    "🦌",
+    "🦌",
+    "🦦",
+    "🦦",
+];
+
+let openCards = [];
+
+let shuffleEmojis = animalsEmojis.sort(() => (
+    Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < animalsEmojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+};
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+};
+
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === animalsEmojis.length) {
+        alert("Você venceu! Parabéns!")
+    }
+}
